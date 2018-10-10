23f2019291d177bbd03fc5b3cac0a29579fc26f5
s20161104566 <a20161104566@mac04.local>
2018-10-10 10:29:14 +0800

    print
    输出数字

--- xiaobenzhu/ViewController.swift
+++ xiaobenzhu/ViewController.swift
@@ -10,6 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var print: UITextField!
+    @IBAction func one(_ sender: Any) {
+        print.text = print.text! + "1"
+    }
+    @IBAction func two(_ sender: Any) {
+        print.text = print.text! + "2"
+    }
+    @IBAction func three(_ sender: Any) {
+        print.text = print.text! + "3"
+        
+    }
+    @IBAction func four(_ sender: Any) {
+        print.text = print.text! + "4"
+        
+    }
+    @IBAction func five(_ sender: Any) {
+        print.text = print.text! + "5"
+        
+    }
+    @IBAction func six(_ sender: Any) {
+        print.text = print.text! + "6"
+        
+    }
+    @IBAction func seven(_ sender: Any) {
+        print.text = print.text! + "7"
+        
+    }
+    @IBAction func eight(_ sender: Any) {
+        print.text = print.text! + "8"
+        
+    }
+    @IBAction func nine(_ sender: Any) {
+        print.text = print.text! + "9"
+        
+    }
+    @IBAction func zero(_ sender: Any) {
+        print.text = print.text! + "0"
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
