b7fd6909c796f295b3d45e31907b5808970e07fd
s20161104566 <a20161104566@mac04.local>
2018-12-26 18:43:10 +0800

    fuhao
    Fuhao

--- xiaobenzhu/ViewController.swift
+++ xiaobenzhu/ViewController.swift
@@ -14,139 +14,252 @@ class ViewController: UIViewController {
     var b = 0.0
     var c = 0.0
     var p = 0
+    var n = ""
+    var m = ""
     @IBOutlet weak var print1: UITextField!
     @IBOutlet weak var print2: UITextField!
-    @IBOutlet weak var print: UITextField!
 
-    @IBAction func one(_ sender: Any) {
-        print.text = print.text! + "1"
+    var re = 0
+    @IBAction func one(_ sender: Any)
+    {
+        if re == 1{
+            
+            print2.text = "1"
+        }
+        else{
+            
+            print2.text = print2.text! + "1"
+            }
     }
-    @IBAction func two(_ sender: Any) {
-        print.text = print.text! + "2"
+    @IBAction func two(_ sender: Any)
+    {
+        if re == 1{
+            
+            print2.text = "2"
+        }
+        else{
+            
+            print2.text = print2.text! + "2"
+        }
     }
-    @IBAction func three(_ sender: Any) {
-        print.text = print.text! + "3"
+    @IBAction func three(_ sender: Any)
+    {
+        if re == 1{
+            
+            print2.text = "3"
+        }
+        else{
+            
+            print2.text = print2.text! + "3"
+        }
         
     }
-    @IBAction func four(_ sender: Any) {
-        print.text = print.text! + "4"
+    @IBAction func four(_ sender: Any)
+    {
+        if re == 1{
+            
+            print2.text = "4"
+        }
+        else{
+            
+            print2.text = print2.text! + "4"
+        }
         
     }
-    @IBAction func five(_ sender: Any) {
-        print.text = print.text! + "5"
+    @IBAction func five(_ sender: Any)
+    {
+        if re == 1{
+            
+            print2.text = "5"
+        }
+        else{
+            
+            print2.text = print2.text! + "5"
+        }
         
     }
-    @IBAction func six(_ sender: Any) {
-        print.text = print.text! + "6"
+    @IBAction func six(_ sender: Any)
+    {
+        if re == 1{
+            
+            print2.text = "6"
+        }
+        else{
+            
+            print2.text = print2.text! + "6"
+        }
         
     }
-    @IBAction func seven(_ sender: Any) {
-        print.text = print.text! + "7"
+    @IBAction func seven(_ sender: Any)
+    {
+        if re == 1{
+            
+            print2.text = "7"
+        }
+        else{
+            
+            print2.text = print2.text! + "7"
+        }
         
     }
-    @IBAction func eight(_ sender: Any) {
-        print.text = print.text! + "8"
+    @IBAction func eight(_ sender: Any)
+    {
+        if re == 1{
+            
+            print2.text = "8"
+        }
+        else{
+            
+            print2.text = print2.text! + "8"
+        }
         
     }
-    @IBAction func nine(_ sender: Any) {
-        print.text = print.text! + "9"
+    @IBAction func nine(_ sender: Any)
+    {
+        if re == 1{
+            
+            print2.text = "9"
+        }
+        else{
+            
+            print2.text = print2.text! + "9"
+        }
         
     }
-    @IBAction func zero(_ sender: Any) {
-        if print.text == ""{
-            print.text = "0"
-        }else{
-            print.text = print.text! + "0"
+    @IBAction func zero(_ sender: Any)
+    {
+        if re == 1{
+            
+            print2.text = "0"
+        }
+        else{
+            
+            print2.text = print2.text! + "0"
         }
         
     }
     @IBAction func result(_ sender: Any) {
-        b = Double(print.text!)!
-        print2.text = String(b)
-        print.text = "0"
-        if number == 1{
+        b = Double(print2.text!)!
+        print1.text = print1.text! + String(b)
+        print2.text = "0"
+        if number == 1
+        {
              c = a + b
-        }else if number == 2{
-            c = a - b
-        }else if number == 3{
-            c = a * b
-        }else if number == 4{
-            c = a / b
         }
-        print.text = String(format:"%.2lf" , c)
+        else if number == 2
+        {
+            c = a - b
+        }
+        else if number == 3
+        {
+            c = a * b
+        }
+        else if number == 4
+        {
+            c = a / (b)
+        }
+       
+        print1.text = print1.text! + "="
+        
+        print2.text = String(format:"%.2lf" , c)
     }
-    @IBAction func point(_ sender: Any) {
-        if p == 0{
-            print.text = print.text! + "."
+    @IBAction func point(_ sender: Any)
+    {
+        if re == 0{
+            print2.text = print2.text! + "."
             p = 1
-        } 
+        }
     }
-    @IBAction func add(_ sender: Any) {
-        if print.text == ""{
-            print.text = "0"
+    @IBAction func add(_ sender: Any)
+    {
+        if print2.text == ""
+        {
+            print2.text = "0"
             number = 1
-        }else{
-            a = Double(print.text!)!
-            print1.text = String(a)
-            print.text = ""
+        }
+        else
+        {
+            a = Double(print2.text!)!
+            print1.text = print2.text! + "+"
+            
+            print2.text = ""
             number = 1
-            p = 0
+            re = 0
         }
     }
     @IBAction func subtraction(_ sender: Any) {
-        if print.text == ""{
-            print.text = "0"
+        if print2.text == ""
+        {
+            print2.text = "0"
             number = 2
-        }else{
-            a = Double(print.text!)!
-            print1.text = String(a)
-            print.text = ""
+        }
+        else
+        {
+            a = Double(print2.text!)!
+            print1.text = print2.text! + "-"
+            
+            print2.text = ""
             number = 2
-            p = 0
+            re = 0
         }
     }
     @IBAction func time(_ sender: Any) {
-        if print.text == ""{
-            print.text = "0"
+        if print2.text == ""
+        {
+            print2.text = "0"
             number = 3
-        }else{
-            a = Double(print.text!)!
-            print1.text = String(a)
-            print.text = ""
+        }
+        else
+        {
+            a = Double(print2.text!)!
+            print1.text = print2.text! + "*"
+            
+            print2.text = ""
             number = 3
-            p = 0
+            re = 0
         }
     }
     
     @IBAction func divide(_ sender: Any) {
-        if print.text == ""{
-            print.text = "0"
+        if print2.text == ""
+        {
+            print2.text = "0"
             number = 4
-        }else{
-            a = Double(print.text!)!
-            print1.text = String(a)
-            print.text = ""
+        }
+        else
+        {
+            a = Double(print2.text!)!
+            print1.text = print2.text! + "/"
+            
+            print2.text = ""
             number = 4
-            p = 0
+            re = 0
         }
     }
     @IBAction func sign(_ sender: Any) {
-        if print.text?.first == "-"{
-            print.text?.removeFirst()
+        if print2.text?.first == "-"
+        {
+            print2.text?.removeFirst()
         }
-        else{
-            print.text = "-" + print.text!
+        else
+        {
+            print2.text = "-" + print2.text!
         }
     }
-    @IBAction func Dele(_ sender: Any) {
-        print.text = ""
+
+    @IBAction func Dele(_ sender: Any)
+    {
+       // print.text = ""
+        n = ""
         print1.text = ""
         print2.text = ""
-        p = 0
+        re = 0
     }
-    @IBAction func C(_ sender: Any) {
-        if print.text != ""{
-            print.text?.removeLast()
+    @IBAction func C(_ sender: Any)
+    {
+        if print2.text != ""
+        {
+            print2.text?.removeLast()
         }
     }
     override func viewDidLoad() {
